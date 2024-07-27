e922043bee5a0f104a6d501332473f83aff83b98
koyakei <koyakeiaaaaa@gmail.com>
2024-07-27 21:21:21 +0000

    タグ追加シリアライザー名変更

--- app/serializers/rest/k_tag_add_relation_request_serializer.rb
+++ app/serializers/rest/k_tag_add_relation_request_serializer.rb
@@ -2,12 +2,29 @@
 
 class REST::KTagAddRelationRequestSerializer < ActiveModel::Serializer
 
-  attributes  :id, :account, :status_id, :k_tag_id, :k_tag, :is_owned, :account_id
+  attributes :id, :is_owned, :decision_status, :k_tag_id, :status_id, :request_comment
 
-  belongs_to :account, serializer: REST::AccountSerializer
-  belongs_to :k_tag
+  belongs_to :requester, class_name: 'Account', foreign_key: :requester_id ,serializer: REST::AccountSerializer
+  belongs_to :k_tag, serializer: REST::KTagSerializer
+  # belongs_to :status, serializer: REST::StatusSerializer
+
+  def id
+    object.id.to_s
+  end
+
+  def k_tag_id
+    object.k_tag.id.to_s
+  end
+
+  def status_id
+    object.status.id.to_s
+  end
 
   def is_owned
-    current_user.account_id == account_id
+    current_user&.account_id == object.requester_id
+  end
+
+  def decision_status
+    object.read_attribute_for_database(:decision_status)
   end
 end
