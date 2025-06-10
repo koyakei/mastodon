e224adc5897fe465dd03ed84bcd74cbcf49c95ca
keisuke koyanagi <koyakeiaaaaa@gmail.com>
2025-06-10 18:11:55 +0900

    アバター表示可能

--- app/javascript/mastodon/features/notifications_v2/components/notification_k_tag_add_relation_request.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/notification_k_tag_add_relation_request.tsx
@@ -2,6 +2,8 @@ import { FormattedMessage } from 'react-intl';
 
 import ReplyIcon from '@/material-icons/400-24px/reply-fill.svg?react';
 import api from 'mastodon/api';
+import { Avatar } from 'mastodon/components/avatar';
+import { DisplayName } from 'mastodon/components/display_name';
 import type { NotificationGroupKTagAddRelationRequest } from 'mastodon/models/notification_group';
 
 import type { LabelRenderer } from './notification_group_with_status';
@@ -16,6 +18,7 @@ export const NotificationKTagAddRelationRequest: React.FC<{
   unread: boolean;
 }> = ({ notification, unread }) => {
   const kTagAddRelationRequest = notification.k_tag_add_relation_request
+
   function approveRequest(){
     api().post(`/api/v1/k_tag_add_relation_requests/${kTagAddRelationRequest.id}/approve`).then(response => {
       // リクエストが受け付けられたら　request_status に合わせてボタンの上体を更新
@@ -25,18 +28,34 @@ export const NotificationKTagAddRelationRequest: React.FC<{
     });
   }
   function denyRequest(){
+    api().post(`/api/v1/k_tag_add_relation_requests/${kTagAddRelationRequest.id}/deny`).then(response => {
+      // リクエストが受け付けられたら　request_status に合わせてボタンの上体を更新
+      request_status = response.data.request_status
+    }).catch(error => {
 
+    });
   }
 
   return (
     <div>
+
+
       {kTagAddRelationRequest.request_status === 0 && (
+
         <div>
+          <div>
+            <div className='account__avatar-wrapper'>
+                        <Avatar withLink account={kTagAddRelationRequest.requester as Account} size={36} />
+                      </div>
+
+                        {kTagAddRelationRequest.requester.acct}
+
+          </div>
           <button onClick={approveRequest}>
-        承諾
+            承諾
           </button>
           <button onClick={denyRequest}>
-        拒否
+            拒否
           </button>
         </div>
       )}
