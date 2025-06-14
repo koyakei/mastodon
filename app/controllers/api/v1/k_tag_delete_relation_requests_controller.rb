0dd9fd3ffb64a0b38cd58aa39d90e4526acdf6e5
keisuke koyanagi <koyakeiaaaaa@gmail.com>
2025-06-14 22:44:49 +0900

    refactor delete request　

--- app/controllers/api/v1/k_tag_delete_relation_requests_controller.rb
+++ app/controllers/api/v1/k_tag_delete_relation_requests_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::KTagDeleteRelationRequestsController < Api::BaseController
   include Authorization
 
-  before_action :set_api_v1_k_tag_delete_relation_request, only: %i[ show approve deny destroy ]
+  before_action :set_api_v1_k_tag_delete_relation_request, only: %i(show approve deny destroy)
   before_action -> { authorize_if_got_token! :read, :'read:statuses' }, except: [:create, :approve, :deny, :destroy]
   before_action -> { doorkeeper_authorize! :write, :'write:statuses' }, only:   [:create, :approve, :deny, :destroy]
-  before_action :require_user!, except:      [:index, :show]
+  before_action :require_user!, except: [:index, :show]
   #  This API was originally unlimited, pagination cannot be introduced without
   # breaking backwards-compatibility. Arbitrarily high number to cover most
   # conversations as quasi-unlimited, it would be too much work to render more
@@ -22,58 +22,54 @@ class Api::V1::KTagDeleteRelationRequestsController < Api::BaseController
 
   # POST /api/v1/k_tag_delete_relation_request
   def create
-    logger.debug KTagRelation.find_by(
-      k_tag_id: api_v1_k_tag_delete_relation_request_params[:k_tag_id],
-       status_id: api_v1_k_tag_delete_relation_request_params[:status_id])
     k_tag_relation = KTagRelation.find_by(
       k_tag_id: api_v1_k_tag_delete_relation_request_params[:k_tag_id],
-       status_id: api_v1_k_tag_delete_relation_request_params[:status_id])
+      status_id: api_v1_k_tag_delete_relation_request_params[:status_id]
+    )
     if k_tag_relation.nil?
-      render json: { error: 'KTagRelation not found' }, status: :not_found
+      render json: { error: 'KTagRelation not found' }, status: 404
     elsif k_tag_relation&.account_id == current_user&.account_id
       # 削除された場合で二重二リクエストが来た場合、自分のものなのに削除リクエストが入る
       if k_tag_relation.destroy
-          UpdateStatusService.new.call(
-            k_tag_relation.status,
+        UpdateStatusService.new.call(
+          k_tag_relation.status,
           current_user.account_id,
           k_tag: true
         )
         # 自分のものを削除できました　レスポンス　http 200 で返すべき　KTagRelation削除メソッドにクライアント側からアクセスするべきなのかも
-        render json: k_tag_relation.status, status: :ok, serializer: REST::StatusSerializer
+        render json: k_tag_relation.status, status: 200, serializer: REST::StatusSerializer
       else
-        render json: k_tag_relation.status, status: :not_found, serializer: REST::StatusSerializer
+        render json: k_tag_relation.status, status: 404, serializer: REST::StatusSerializer
       end
     else
       # 他人の所有しているタグだった場合リクエストを送る　通知
-      api_v1_k_tag_delete_relation_request = KTagDeleteRelationRequest.new(api_v1_k_tag_delete_relation_request_params.merge(requester_id: current_user.account_id ))
+      api_v1_k_tag_delete_relation_request = KTagDeleteRelationRequest.new(api_v1_k_tag_delete_relation_request_params.merge(requester_id: current_user.account_id))
       if api_v1_k_tag_delete_relation_request.save
         UpdateStatusService.new.call(
           k_tag_relation.status,
           current_user.account_id,
           k_tag: true
         )
-        LocalNotificationWorker.perform_async(k_tag_relation&.account_id, api_v1_k_tag_delete_relation_request.id, 'KTagDeleteRelationRequest','k_tag_delete_relation_request')
-        render json: k_tag_relation.status, status: :ok, serializer: REST::StatusSerializer
+        LocalNotificationWorker.perform_async(k_tag_relation&.account_id, api_v1_k_tag_delete_relation_request.id, 'KTagDeleteRelationRequest', 'k_tag_delete_relation_request')
+        render json: k_tag_relation.status, status: 200, serializer: REST::StatusSerializer
       else
-        render json: { errors: api_v1_k_tag_delete_relation_request.errors.full_messages }, status: :conflict
+        render json: { errors: api_v1_k_tag_delete_relation_request.errors.full_messages }, status: 409
       end
     end
   end
 
   def approve
     authorize @api_v1_k_tag_delete_relation_request, :approve?
-    render json: {error: "already reviewed"}, status: :unprocessable_entity if @api_v1_k_tag_delete_relation_request.reviewed?
+    render json: { error: 'already reviewed' }, status: :unprocessable_entity if @api_v1_k_tag_delete_relation_request.reviewed?
     # already_created?
     if @api_v1_k_tag_delete_relation_request.approved?
-      render json: { error: "already approved" }, status: :unprocessable_entity
+      render json: { error: 'already approved' }, status: :unprocessable_entity
     else
       k_tag_relation = @api_v1_k_tag_delete_relation_request.k_tag_relation
-      if k_tag_relation.discarded?
-        render json: {error: "k tag relation alredy discarded"}, status: :unprocessable_entity
-      end
+      render json: { error: 'k tag relation alredy discarded' }, status: :unprocessable_entity if k_tag_relation.discarded?
       ActiveRecord::Base.transaction do
         # 承認して関係性を削除
-        @api_v1_k_tag_delete_relation_request.update(request_status: :approved, review_comment: params[:review_comment] || "")
+        @api_v1_k_tag_delete_relation_request.update(request_status: :approved, review_comment: params[:review_comment] || '')
 
         k_tag_relation.discard!
         UpdateStatusService.new.call(
@@ -85,36 +81,35 @@ class Api::V1::KTagDeleteRelationRequestsController < Api::BaseController
           account_id: current_user.account_id,
           k_tag_id: @api_v1_k_tag_delete_relation_request.k_tag_relation.k_tag_id,
           status_id: @api_v1_k_tag_delete_relation_request.k_tag_relation.status_id,
-          trade_count: -1)
+          trade_count: -1
+        )
         LocalNotificationWorker.perform_async(k_tag_relation.account_id,
-        @api_v1_k_tag_delete_relation_request.id , 'KTagDeleteRelationRequest', 'k_tag_appproved_delete_relation_request')
-      rescue ActiveRecord::RecordInvalid => exception
-        render json: {error: exception}, status: :unprocessable_entity
+                                              @api_v1_k_tag_delete_relation_request.id, 'KTagDeleteRelationRequest', 'k_tag_appproved_delete_relation_request')
+      rescue ActiveRecord::RecordInvalid => e
+        render json: { error: e }, status: :unprocessable_entity
       end
-      render json: @api_v1_k_tag_delete_relation_request, status: :ok
+      render json: @api_v1_k_tag_delete_relation_request, status: 200
     end
   end
 
   def deny
     authorize @api_v1_k_tag_delete_relation_request, :deny?
-    render json: {error: "already reviewed"}, status: :unprocessable_entity if @api_v1_k_tag_delete_relation_request.reviewed?
+    render json: { error: 'already reviewed' }, status: :unprocessable_entity if @api_v1_k_tag_delete_relation_request.reviewed?
     if @api_v1_k_tag_delete_relation_request.denied?
-      render json: { error: "already denied" }, status: :unprocessable_entity
+      render json: { error: 'already denied' }, status: :unprocessable_entity
+    elsif @api_v1_k_tag_delete_relation_request.update(request_status: :denied, review_comment: paarams[:review_comment] || '')
+      @api_v1_k_tag_delete_relation_request.k_tag_relation.undiscard ## error catch しなくていいの？　過去に一回削除承認しても拒否で戻せるように　押し間違い対応
+      LocalNotificationWorker.perform_async(k_tag_relation.account_id,
+                                            @api_v1_k_tag_delete_relation_request.id, 'KTagDeleteRelationRequest', 'k_tag_denied_delete_relation_request')
+      UpdateStatusService.new.call(
+        @api_v1_k_tag_delete_relation_request.k_tag_relation.status,
+        current_user.account_id,
+        k_tag: true
+      )
+      render json: @api_v1_k_tag_delete_relation_request
+    # TODO: 残りのリクエストを全部同じ決定にして処理するの？
     else
-      if @api_v1_k_tag_delete_relation_request.update(request_status: :denied, review_comment: paarams[:review_comment]|| "")
-        #TODO: 残りのリクエストを全部同じ決定にして処理するの？
-        @api_v1_k_tag_delete_relation_request.k_tag_relation.undiscard ## error catch しなくていいの？　過去に一回削除承認しても拒否で戻せるように　押し間違い対応
-        LocalNotificationWorker.perform_async(k_tag_relation.account_id,
-        @api_v1_k_tag_delete_relation_request.id, 'KTagDeleteRelationRequest', 'k_tag_denied_delete_relation_request')
-        UpdateStatusService.new.call(
-          @api_v1_k_tag_delete_relation_request.k_tag_relation.status,
-          current_user.account_id,
-          k_tag: true
-        )
-        render json: @api_v1_k_tag_delete_relation_request
-      else
-        render :edit, status: :unprocessable_entity
-      end
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -122,18 +117,18 @@ class Api::V1::KTagDeleteRelationRequestsController < Api::BaseController
   def destroy
     authorize @api_v1_k_tag_delete_relation_request, :destroy?
     api_v1_k_tag_delete_relation_request.destroy!
-    redirect_to api_v1_k_tag_add_relation_request_url, notice: "K tag add relation reque was successfully destroyed.", status: :see_other
+    redirect_to api_v1_k_tag_add_relation_request_url, notice: 'K tag add relation reque was successfully destroyed.', status: 303
   end
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_k_tag_delete_relation_request
-      @api_v1_k_tag_delete_relation_request = KTagDeleteRelationRequest.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_api_v1_k_tag_delete_relation_request
+    @api_v1_k_tag_delete_relation_request = KTagDeleteRelationRequest.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def api_v1_k_tag_delete_relation_request_params
-      params.permit( :status_id, :k_tag_relation_id,:k_tag_id, :request_comment, :review_comment)
-    end
+  # Only allow a list of trusted parameters through.
+  def api_v1_k_tag_delete_relation_request_params
+    params.permit(:status_id, :k_tag_relation_id, :k_tag_id, :request_comment, :review_comment)
+  end
 end
