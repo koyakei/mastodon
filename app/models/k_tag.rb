a588d317cb4088863329a539b0b11059c9b0aebb
keisuke koyanagi <koyakeiaaaaa@gmail.com>
2025-07-02 20:49:05 +0900

    dependency 推奨なので一応入れた。

--- app/models/k_tag.rb
+++ app/models/k_tag.rb
@@ -12,17 +12,17 @@
 #
 class KTag < ApplicationRecord
   belongs_to :account
-  has_many :k_tag_relations
+  has_many :k_tag_relations, dependent: :destroy
   has_many :statuses, through: :k_tag_relations
   has_many :followers, through: :k_tag_follows, source: :account
-  has_many :k_tag_trading_history
+  has_many :k_tag_trading_history, dependent: :destroy
   attribute :followers_count, :integer, default: 0
 
   validates :followers_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   scope :matches_name, ->(term) { where(arel_table[:name].lower.matches(arel_table.lower("#{sanitize_sql_like(KTag.normalize(term))}%"), nil, true)) } # Search with case-sensitive to use B-tree index
   update_index('k_tags', :self)
   update_index('statuses', :statuses)
-  has_many :k_tag_delete_relation_requests
+  has_many :k_tag_delete_relation_requests, dependent: :destroy
 
   def increment_follower_count!
     update(followers_count: following_count + 1)
